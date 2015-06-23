1d102c664460c60f0d0107c4c7d2c698e5bbe5e2
sidx <sidharth.xtb@gmail.com>
2015-06-23 20:03:50 +0530

    Corrected

--- bin/app.py
+++ bin/app.py
@@ -1,15 +1,27 @@
 import web
 
 urls = (
-	'/', 'index'
+	'/', 'Index'
+	
 	)
 
 app = web.application(urls, globals())
 
-class index:
+render = web.template.render('templates/')
+
+class Index(object):
 	def GET(self):
-		greeting = "Hello World"
-		return greeting
+		greeting = "Hello ssWorld"
+		return render.index(greeting = greeting)
+
+
+'''class first:
+	"""docstring for first"""
+	def GET(self):
+		message = "First page"
+		return render.index(greeting = greeting)'''
+		
+
 
 if __name__ == "__main__":
 	app.run()
